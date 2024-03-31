334614119435bf0c4aec6daff081bbbaa7e81ee3
BrittoT <meetbrittohere@gmail.com>
2024-03-31 22:58:46 +0530

    Login form changed

--- ClientApp/src/components/Login.tsx
+++ ClientApp/src/components/Login.tsx
@@ -10,51 +10,77 @@ const Login = ()=>{
     }
 
 
-    return(
-        <form onSubmit={handleSubmit}>
-        <h3>Sign In</h3>
+  return (
+    <>
+      <form onSubmit={handleSubmit} >
+        <div className="container">
+          <div className="row">
 
-        <div className="mb-3">
-          <label>Email address</label>
-          <input
-            type="email"
-            className="form-control"
-            placeholder="Enter email"
-          />
-        </div>
+            <div className="col">
+             
+            </div>
 
-        <div className="mb-3">
-          <label>Password</label>
-          <input
-            type="password"
-            className="form-control"
-            placeholder="Enter password"
-          />
-        </div>
+            <div className="col g-3">
+              <div className="container px-4">
+                <div className="row gx-5">
+                  <div className="p-3 border bg-light">
 
-        <div className="mb-3">
-          <div className="custom-control custom-checkbox">
-            <input
-              type="checkbox"
-              className="custom-control-input"
-              id="customCheck1"
-            />
-            <label className="custom-control-label" htmlFor="customCheck1">
-              Remember me
-            </label>
-          </div>
-        </div>
+                    <h3>Sign In</h3>
 
-        <div className="d-grid">
-          <button type="submit" className="btn btn-primary">
-            Submit
-          </button>
-        </div>
-        <p className="forgot-password text-right">
-          Forgot <a href="#">password?</a>
-        </p>
+                    <div className="mb-3 position-relative">
+                      <label htmlFor="validationTooltip01" className="form-label">Email address</label>
+                      <input type="email" className="form-control" id="validationTooltip01" required placeholder="Enter email" />
+                      <div className="invalid-tooltip">
+                        Please provide a valid Email address.
+                      </div>
+                    </div>
+
+                    <div className="mb-3 position-relative">
+                      <label htmlFor="validationTooltip02" className="form-label">Password</label>
+                      <input type="password" className="form-control" id="validationTooltip02" required placeholder="Enter password" />
+                      <div className="invalid-tooltip">
+                        Please provide a valid Password.
+                      </div>
+                    </div>
+
+
+                    <div className="mb-3">
+                      <div className="form-check">
+                        <input className="form-check-input" type="checkbox" value="" id="invalidCheck" required />
+                        <label className="form-check-label" htmlFor="invalidCheck">
+                          Agree to terms and conditions
+                        </label>
+                        <div className="invalid-feedback">
+                          You must agree before submitting.
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="d-grid">
+                      <button className="btn btn-primary" type="submit">Submit form</button>
+                    </div>
+
+                    <p className="forgot-password text-right">
+                      Forgot <a href="#">password?</a>
+                    </p>
+
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="col">
+             
+            </div>
+
+          </div> {/* row */}
+        </div>  {/* container */}
+
       </form>
-    )
+
+
+    </>
+  )
 }
 
 export default Login
